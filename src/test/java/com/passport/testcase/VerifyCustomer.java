287513cd62325510d3bce6dced6959e19ac0427b
vinoth <vinothkgs18@gmail.com>
2023-09-20 22:48:59 +0530

    modified

--- src/test/java/com/passport/testcase/VerifyCustomer.java
+++ src/test/java/com/passport/testcase/VerifyCustomer.java
@@ -4,18 +4,27 @@ import static org.testng.Assert.ARRAY_MISMATCH_TEMPLATE;
 
 import org.openqa.selenium.By;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.passport.Base.TestBase;
+import com.passport.utilities.Utillities;
 
 public class VerifyCustomer extends TestBase{
 
 	
-	@Test
-	public void searchCustomer() {
+	@Test(dataProvider="getData")
+	public void searchCustomer(String name) {
 		driver.findElement(By.cssSelector(OR.getProperty("ShowCustomerButton"))).click();
-		driver.findElement(By.cssSelector(OR.getProperty("SearchCustomer"))).sendKeys("Vinothkumar");
+		driver.findElement(By.cssSelector(OR.getProperty("SearchCustomer"))).clear();
+		driver.findElement(By.cssSelector(OR.getProperty("SearchCustomer"))).sendKeys(name);
 		System.out.println(driver.findElement(By.xpath(OR.getProperty("CustomerData"))).getText());
-		Assert.assertEquals("Vinothkumar", driver.findElement(By.xpath(OR.getProperty("CustomerData"))).getText());
+		Assert.assertEquals(name, driver.findElement(By.xpath(OR.getProperty("CustomerData"))).getText());
+	}
+	
+	@DataProvider
+	public Object[][] getData(){
+		String[][] data = Utillities.getDataFromExcel("./src/test/resources/excel/AddCustomerDetails.xlsx", 2);
+		return data;
 	}
 }
